f16188a41da7cff48a635823497bfd2d2d9d0e8f
sofiaggb <sofiaggb@gmail.com>
2026-06-21 18:13:18 -0400

    email pr ipv4

--- backend/src/config/email.js
+++ backend/src/config/email.js
@@ -6,7 +6,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
